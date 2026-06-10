a3efc48c5bcb558897883a954b0bd8227850c2af
luizryke <luizhenr.contact@gmail.com>
2026-06-10 01:06:58 -0300

    Add unit tests with JUnit

---
+++ src/test/java/com/monitoring/sales/OrderTest.java
@@ -0,0 +1,26 @@
+package com.monitoring.sales;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class OrderTest {
+    
+    @Test
+    void shouldCalculateTotalWhenOrderHasMultipleItems() {
+
+        Order order = new Order(
+                "John Doe",
+                List.of(
+                        new OrderItem("Keyboard", 100, 2),
+                        new OrderItem("Mouse", 50, 1)
+                )
+        );
+
+        double total = order.calculateTotal();
+
+        assertEquals(250, total);
+    }
+}
